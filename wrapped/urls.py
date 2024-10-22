4ed96aad0f5bfa705181f34f368432071965b60e
AbdullahLightningNinja <mkfamilysp@gmail.com>
2024-10-22 01:17:51 -0400

    initial pages and basic site backbone created

---
+++ wrapped/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from django.contrib import admin
+
+from wrapped import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('/about/', views.about, name='about'),
+    path('/contact/', views.contact, name='contact'),
+    path('/wrapper', views.wrapper, name='wrapper'),
+]
--- wrapped/views.py
+++ wrapped/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+def wrapper(request):
+    return render(request, 'wrapper.html')
