b11d4538239f34db94e6e6cc1941a4bb4dc14bbc
AbdullahLightningNinja <mkfamilysp@gmail.com>
2024-11-21 11:55:02 -0500

    redid login layout (2-step)

--- wrapped/models.py
+++ wrapped/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class SpotifyWrap(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    wrap_data = models.JSONField()  # Store the wrap data as JSON
+    top_tracks = models.JSONField()  # Stores top tracks data as a JSON
+    top_artists = models.JSONField()  # Stores top artists data as a JSON
+    top_genres = models.JSONField()  # Stores top genres data as a JSON
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Wrap for {self.user.username} - {self.created_at.strftime('%Y-%m-%d')}"
+
