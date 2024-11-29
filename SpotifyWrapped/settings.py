fbe628b7c52634584fa9fe1ad60024497ac3d0f8
nabhan-s <NABHAN.SOPHIE@GMAIL.COM>
2024-11-28 19:16:08 -0500

    fix for staticfiles_dir
    this commit should make the last one unnecessary, while keeping css intact

--- SpotifyWrapped/settings.py
+++ SpotifyWrapped/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) # test
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'wrapped/static'),) # test
 django_heroku.settings(locals())
 
 # Default primary key field type
