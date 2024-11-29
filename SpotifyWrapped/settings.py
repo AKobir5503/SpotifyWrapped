faabc11685837bf9bd4d574c3c8ff022638ff3f4
nabhan-s <NABHAN.SOPHIE@GMAIL.COM>
2024-11-29 17:06:29 -0500

    reverting to what works
    used to work before youssef/my conflicts

--- SpotifyWrapped/settings.py
+++ SpotifyWrapped/settings.py
@@ -6,22 +6,36 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY', config('SECRET_KEY')) #config('SECRET_KEY', default='your_default_secret_key')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', config('DEBUG', default=False))
+# Retrieve the variables from the environment
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+
+"""
 # Environment variables
 SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key')
 DEBUG = config('DEBUG', default=False, cast=bool)
 SPOTIFY_CLIENT_ID = config('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = config('SPOTIFY_CLIENT_SECRET')
 
+
 print(f"SECRET_KEY: {config('SECRET_KEY', default='MISSING')}")
 print(f"DEBUG: {config('DEBUG', default='MISSING', cast=bool)}")
 print(f"SPOTIFY_CLIENT_ID: {config('SPOTIFY_CLIENT_ID', default='MISSING')}")
 print(f"SPOTIFY_CLIENT_SECRET: {config('SPOTIFY_CLIENT_SECRET', default='MISSING')}")
+"""
 
+ALLOWED_HOSTS = ['*']
+
+"""
 # Adjust ALLOWED_HOSTS
 if DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 else:
     ALLOWED_HOSTS = ['*']
+"""
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -77,6 +91,14 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'wrapped/static'),) # test
+#STATICFILES_DIRS = []
+
+"""
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
@@ -90,6 +112,7 @@ if not DEBUG:  # Use this only in production
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+"""
 
 django_heroku.settings(locals())
 
